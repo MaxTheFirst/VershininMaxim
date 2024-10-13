9a267c01afe83e1208e76e581ffdd6e284623a58
MaxTheFirst <maks.vershinin.2005@mail.ru>
2024-10-13 16:05:04 +0300

    edit pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
-public class Main {
-  public static void main(String[] args) {
+import sort.ElementCountLimitedException;
+import sort.SortManager;
+import sort.SortType;
+import sort.sorters.BubbleSorter;
+import sort.sorters.MergeSorter;
 
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws ElementCountLimitedException {
+    List<Integer> inList = List.of(2, 3, 5, 10, 8, 7);
+    SortManager manager = new SortManager(
+        List.of(
+            new BubbleSorter(),
+            new MergeSorter()
+        )
+    );
+    List<Integer> result = manager.sort(inList, SortType.MERGE);
+    System.out.println(result);
   }
 }
---
+++ src/main/java/sort/SortManager.java
@@ -0,0 +1,24 @@
+package sort;
+
+import java.util.List;
+
+public class SortManager {
+  private final List<BaseSorter> sorters;
+
+  public SortManager(List<BaseSorter> sorters) {
+    this.sorters = sorters;
+  }
+
+  public List<Integer> sort(List<Integer> array, SortType type) throws ElementCountLimitedException {
+    for (BaseSorter sorter : sorters) {
+      if (sorter.type().equals(type)) {
+        try {
+         return sorter.sort(array);
+        } catch (ElementCountLimitedException e) {
+          System.out.println("Couldn`t execute sort. Trying next one...");
+        }
+      }
+    }
+    throw new ElementCountLimitedException("No implementation was suitable due to the large number of elements.");
+  }
+}
