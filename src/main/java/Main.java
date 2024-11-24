47fbc13bda10c7a1cdc20b834c71a48ced12f4a0
MaxTheFirst <maks.vershinin.2005@mail.ru>
2024-11-24 14:48:10 +0300

    add dependence

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
 public class Main {
   public static void main(String[] args) {
-
+    Spark.get("/hello", (Request request, Response response) -> "Hello, World!");
   }
 }
